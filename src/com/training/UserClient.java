1a6482da879c14a64146676b29dc6d5dee5826c5
COMLAB <COMLAB@DESKTOP-7PQ0LLF>
2026-05-20 12:09:02 +0400

    created many users and asked  to take values and displayed 3rd details

--- src/com/training/UserClient.java
+++ src/com/training/UserClient.java
@@ -1,17 +1,36 @@
 package com.training;
 
+import java.util.Scanner;
+
 public class UserClient {
 
     public static void main(String[] args) {
 
-      //  User user1 = new User();
-        User user1=new User("A101","Haitham","abc@gmail.com");
-        //user1.setUserId("A101");
-       // user1.setUserName("Haitham");
-       // user1.setUseEmail("abc@gmail.com");
-        User .organazationName = "OAB";
-        User user2 = new User("A101","Haitham","abc@gmail.com");
-        System.out.println("userID: " + user1.getUserId() +
-                           "\nuserName: " + user1.getUserName()+" "+" Organaization:"+User.organazationName);
+        Scanner sc = new Scanner(System.in);
+
+        // taking input
+        System.out.print("Enter User ID: ");
+        String id = sc.nextLine();
+
+        System.out.print("Enter User Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter User Email: ");
+        String email = sc.nextLine();
+
+        // creating object
+        User user1 = new User(id, name, email);
+
+        // static variable
+        User.organizationName = "OAB";
+
+        // displaying output
+        System.out.println("\nUser Details");
+        System.out.println("User ID: " + user1.getUserId());
+        System.out.println("User Name: " + user1.getUserName());
+        System.out.println("User Email: " + user1.getUseEmail());
+        System.out.println("Organization: " + User.organizationName);
+
+        sc.close();
     }
 }
