6dccea90c2cd04baf6dad5c0cd48964c05c7f520
COMLAB <COMLAB@DESKTOP-7PQ0LLF>
2026-05-20 11:04:16 +0400

    user class intialization

---
+++ src/com/training/User.java
@@ -0,0 +1,34 @@
+package com.training;
+
+public class User {
+	//Attributes
+	private String userId;
+	private String userName;
+	private String useEmail;
+
+	
+		//Behavior
+	//Setter method
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUseEmail() {
+		return useEmail;
+	}
+
+	public void setUseEmail(String useEmail) {
+		this.useEmail = useEmail;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+
+	}
+
