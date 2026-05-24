7070c33d4c2b70e364e8ff51bb0fcd2983715e77
COMLAB <COMLAB@DESKTOP-7PQ0LLF>
2026-05-24 09:44:46 +0400

    overriding equals and Hasshcode

--- src/com/training/CollectionDemo.java
+++ src/com/training/CollectionDemo.java
@@ -1,10 +1,56 @@
 package com.training;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+
 public class CollectionDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Set<String> names =new HashSet<>();
+		names.add("Zara");
+		names.add("Tina");
+		names.add("Sayeed");
+	//	names.add(23);
+		Iterator it = names.iterator();
+		while (it.hasNext()) {
+			String name = (String)it.next();
+		System.out.println(name);
+		
+
 
 	}
+		
+		
+		Set<User>userSet = new HashSet<>();
+		User user1 = new User("A101","Gaith","abc@maol.com");
+		User user2 = new User("A102","Anwar","sdf@maol.com");
+		User user3 = new User("A103","Ayuub","gjf@maol.com");
+		User user4 = new User("A104","Rohan","rho@maol.com");
+		User user5 = new User("A104","Rohan","rho@maol.com");
 
+		userSet.add(user1);
+		userSet.add(user2);
+		userSet.add(user3);
+		userSet.add(user4);
+		userSet.add(user5);
+
+		
+		
+		for(User user:userSet) {
+			System.out.println(user);
+		}
+
+
+		
+		
+		
+
+		
+		
+		
+		
+		
+	}
 }
--- src/com/training/User.java
+++ src/com/training/User.java
@@ -1,5 +1,7 @@
 package com.training;
 
+import java.util.Objects;
+
 //Encapsulation
 public class User{
 	
@@ -43,6 +45,24 @@ public class User{
 	@Override
 	public String toString() {
 		return "User [userId=" + userId + ", userName=" + userName + ", userEmail=" + userEmail + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(userEmail, userId, userName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(userEmail, other.userEmail) && Objects.equals(userId, other.userId)
+				&& Objects.equals(userName, other.userName);
 	}
 	
 	
