9fb97a275d285ab764854176fba9828883a78818
unknown <you@example.com>
2026-06-11 11:26:34 +0400

    Initial commit

---
+++ src/main/java/com/training/accountsvc/controller/AccountController.java
@@ -0,0 +1,23 @@
+package com.training.accountsvc.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.training.accountsvc.entity.Account;
+import com.training.accountsvc.service.AccountService;
+
+@RestController
+@RequestMapping("/account")
+public class AccountController {
+	@Autowired
+	private AccountService accountService;
+	
+	@PostMapping
+	public Account AddAccount(@RequestBody Account account) {
+		return accountService.AddAccount(account);
+	}
+
+}
---
+++ src/main/java/com/training/accountsvc/service/AccountService.java
@@ -0,0 +1,8 @@
+package com.training.accountsvc.service;
+
+import com.training.accountsvc.entity.Account;
+
+public interface AccountService {
+	Account AddAccount(Account account);
+
+}
